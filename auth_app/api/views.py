67316fa17eb2c17ede57b53ae85bb9b041f7112f
KKrntzk <sophie.kornetzki@gmail.com>
2026-06-09 12:58:20 +0200

    login view added

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, LoginSerializer
+from rest_framework.authtoken.models import Token
 
 class RegistrationView(APIView):
     
@@ -22,4 +23,25 @@ class RegistrationView(APIView):
             }
 
             return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+    permission_classes=[AllowAny]
+
+    def post(self,request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+
+            token, created = Token.objects.get_or_create(user=user)
+
+            response_data = {
+                "token": token.key,
+                "fullname": user.first_name,  
+                "email": user.email,
+                "user_id": user.id
+            }
+
+            return Response(response_data, status=status.HTTP_200_OK)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
