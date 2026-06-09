43d21dd51c5720428ca9bb7747c8d5f03d527355
KKrntzk <sophie.kornetzki@gmail.com>
2026-06-09 14:33:24 +0200

    boardDetailView added

--- board_app/api/views.py
+++ board_app/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from django.db.models import Q
 from board_app.models import Board
-from .serializers import BoardSerializer
+from .serializers import BoardSerializer, BoardDetailSerializer, BoardUserSerializer
 from .permissions import IsBoardOwnerOrMember
 
 class BoardListView(generics.ListCreateAPIView):
@@ -18,3 +18,14 @@ class BoardListView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class BoardDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsBoardOwnerOrMember]
+    serializer_class = BoardDetailSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Board.objects.filter(Q(owner=user) | Q(members=user)).distinct()
