684c1ab04d362068390eb88382013799b6efcd26
KKrntzk <sophie.kornetzki@gmail.com>
2026-06-09 13:49:31 +0200

    isboardownerormember persmission

---
+++ board_app/api/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework import permissions
+
+class IsBoardOwnerOrMember(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user or obj.members.filter(id=request.user.id).exists()
