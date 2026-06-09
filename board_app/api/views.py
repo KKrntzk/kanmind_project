a44cbcc3c023e0251563647c2c41b68e98807ca0
KKrntzk <sophie.kornetzki@gmail.com>
2026-06-09 14:25:45 +0200

    views refactored to generic views

--- board_app/api/views.py
+++ board_app/api/views.py
@@ -1,6 +1,4 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from django.db.models import Q
@@ -8,23 +6,15 @@ from board_app.models import Board
 from .serializers import BoardSerializer
 from .permissions import IsBoardOwnerOrMember
 
-class BoardListView(APIView):
+class BoardListView(generics.ListCreateAPIView):
+
+    serializer_class = BoardSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated, IsBoardOwnerOrMember]
 
-    def get(self, request):
-        user = request.user
-        boards = Board.objects.filter(Q(owner=user) | Q(members=user)).distinct()
+    def get_queryset(self):
+        user = self.request.user
+        return Board.objects.filter(Q(owner=user) | Q(members=user)).distinct()
 
-        serializer = BoardSerializer(boards, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self, request):
-        serializer = BoardSerializer(data=request.data)
-        
-        if serializer.is_valid():
-            board = serializer.save(owner=request.user)
-
-            return Response(BoardSerializer(board).data, status=status.HTTP_201_CREATED)
-            
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
