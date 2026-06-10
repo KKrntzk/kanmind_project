e0751772f0cb9953fe7f49cbb069ecdeb3f2cd62
KKrntzk <sophie.kornetzki@gmail.com>
2026-06-10 12:35:52 +0200

    url and views updated task create

--- task_app/api/urls.py
+++ task_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AssignedToMeTaskListView, ReviewingTaskListView
+from .views import AssignedToMeTaskListView, ReviewingTaskListView, TaskCreateView
 
 urlpatterns = [
     path('tasks/assigned-to-me/', AssignedToMeTaskListView.as_view(), name='tasks-assigned-to-me'),
     path('tasks/reviewing/', ReviewingTaskListView.as_view(), name='tasks-reviewing'),
+    path('tasks/', TaskCreateView.as_view(), name='task-create'),
 ]
--- task_app/api/views.py
+++ task_app/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from task_app.models import Task
-from .serializers import AssignedTaskSerializer
+from .serializers import AssignedTaskSerializer, TaskCreateSerializer
 
 class AssignedToMeTaskListView(generics.ListAPIView):
     authentication_classes = [TokenAuthentication]
@@ -22,3 +22,10 @@ class ReviewingTaskListView(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return Task.objects.filter(reviewer=user)
+    
+
+class TaskCreateView(generics.CreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Task.objects.all()
+    serializer_class = TaskCreateSerializer
