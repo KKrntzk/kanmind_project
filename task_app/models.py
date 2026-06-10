28395c29f18f257173540e9329066abba37fc50b
KKrntzk <sophie.kornetzki@gmail.com>
2026-06-10 11:59:34 +0200

    tasks model added

--- task_app/models.py
+++ task_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from board_app.models import Board
 
 # Create your models here.
+
+class Task(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        TODO = 'to-do', 'To Do'
+        IN_PROGRESS = 'in-progress', 'In Progress'
+        REVIEW = 'review', 'Review'  
+        DONE = 'done', 'Done'
+
+    class PriorityChoices(models.TextChoices):
+        LOW = 'low', 'Low'
+        MEDIUM = 'medium', 'Medium'
+        HIGH = 'high', 'High'
+
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    
+    
+    status = models.CharField(
+        max_length=20, 
+        choices=StatusChoices.choices, 
+        default=StatusChoices.TODO
+    )
+    priority = models.CharField(
+        max_length=20, 
+        choices=PriorityChoices.choices, 
+        default=PriorityChoices.MEDIUM
+    )
+    
+    assignee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_tasks')
+    reviewer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='reviewed_tasks')
+    due_date = models.DateField(null=True, blank=True)
+    comments_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.title} ({self.status})"
