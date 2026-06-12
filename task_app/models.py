56758a68df62b50d968d5ece306a133a5b57bbc7
KKrntzk <sophie.kornetzki@gmail.com>
2026-06-12 08:49:46 +0200

    comment model implemented

--- task_app/models.py
+++ task_app/models.py
@@ -41,3 +41,18 @@ class Task(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.status})"
+    
+
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on Task {self.task.id}"
